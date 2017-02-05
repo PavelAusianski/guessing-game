398b92ac47bedb6501acf143b24f49f15e573869
PavelAusianski <auspavel29@gmail.com>
2017-02-05 19:06:48 +0300

    guessing-game: impl

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,36 @@
 class GuessingGame {
-    constructor() {}
-
+ 
+    constructor() {
+        //left index of range
+        this.min = 0;
+        //right index of range
+        this.max = 0;
+    }
+ 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
+ 
     guess() {
-
+        return this.middleOfRange();
     }
-
+ 
     lower() {
-
+        this.max = this.middleOfRange();
     }
-
+ 
     greater() {
-
+        this.min = this.middleOfRange();
+    }
+ 
+    /**
+     * find index of the middle of range
+     * @returns {number} index of the middle of current range
+     */
+    middleOfRange() {
+        return Math.ceil((this.min + this.max) / 2);
     }
 }
-
+ 
 module.exports = GuessingGame;
